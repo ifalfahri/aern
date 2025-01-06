287de99f8771349625ea9cad386c6bb0181b941b
IfalEXZ <ifaltampan69@gmail.com>
2025-01-06 11:40:55 +0800

    refactor: clean up Home component and enhance Navbar styles for better user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,35 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import { LoadingScreen } from '@/components/loading-screen'
-import { Navbar } from '@/components/navbar'
+import { LoadingScreen } from "@/components/loading-screen";
+import { Navbar } from "@/components/navbar";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setIsLoading(false), 2000)
-    return () => clearTimeout(timer)
-  }, [])
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   if (isLoading) {
-    return <LoadingScreen />
+    return <LoadingScreen />;
   }
 
   return (
     <div className="max-w-screen min-h-screen flex items-center justify-center bg-gradient-to-br from-primary/20 to-secondary/20">
       <Navbar />
-      
-      {/* <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        
-            <motion.div
-              animate={{
-                scale: [1, 1.05, 1],
-                rotate: [0, 5, -5, 0],
-              }}
-              transition={{
-                duration: 5,
-                ease: "easeInOut",
-                times: [0, 0.2, 0.5, 0.8, 1],
-                repeat: Infinity,
-                repeatDelay: 1
-              }}
-            >
-              <img
-                src="/AernStoreV.svg"
-                alt="AernStore"
-                className='w-60 h-40 md:w-96 md:h-60'
-              />
-            </motion.div>
-            <motion.h1
-              className="text-3xl font-bold text-center text-primary"
-              animate={{ opacity: [1, 0.5, 1] }}
-              transition={{ duration: 2, repeat: Infinity }}
-            >
-              Halaman Utama Belum Jadi
-            </motion.h1>
-            <p className="text-center text-muted-foreground">
-              Aldo masih capek. <a href="https://github.com/ifalfahri" className='text-primary'>#SaveAldo</a>
-            </p>
-      </motion.div> */}
+
+      <main className="container mx-auto flex flex-col items-center justify-center py-6">
+        <h1 className="text-4xl font-bold text-center text-gray-800 dark:text-white">
+          Selamat Datang di AernStore
+        </h1>
+        <p className="mt-4 text-lg text-center text-gray-600 dark:text-gray-400">
+          AernStore merupakan sebuah website dari toko online yang menjual
+          berbagai macam barang dengan harga terjangkau dengan mottonya
+          #SahabatBelanjaHemat.
+        </p>
+      </main>
     </div>
-  )
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -37,9 +37,9 @@ export function Navbar() {
           </button>
         </div>
         <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? '' : 'hidden'}`} id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-normal border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link href="/" className="block py-2 px-3 text-white bg-primary rounded md:bg-transparent md:text-primary md:p-0 md:dark:text-blue-500" aria-current="page">
+              <Link href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Beranda
               </Link>
             </li>
