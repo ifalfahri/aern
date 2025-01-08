9b70a3381aee904b4b0c97fdb8baeb460e19d680
IfalEXZ <ifaltampan69@gmail.com>
2025-01-08 08:49:47 +0800

    feat: add ShuffleHero component and update Navbar links for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { LoadingScreen } from "@/components/loading-screen";
 import { Navbar } from "@/components/navbar";
+import ShuffleHero from "@/components/shuffle-hero";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -17,19 +18,10 @@ export default function Home() {
   }
 
   return (
-    <div className="max-w-screen min-h-screen flex items-center justify-center bg-gradient-to-br from-primary/20 to-secondary/20">
+    <div className="bg-gradient-to-br from-primary/20 to-secondary/20">
       <Navbar />
 
-      <main className="container mx-auto flex flex-col items-center justify-center py-6">
-        <h1 className="text-4xl font-bold text-center text-gray-800 dark:text-white">
-          Selamat Datang di AernStore
-        </h1>
-        <p className="mt-4 text-lg text-center text-gray-600 dark:text-gray-400">
-          AernStore merupakan sebuah website dari toko online yang menjual
-          berbagai macam barang dengan harga terjangkau dengan mottonya
-          #SahabatBelanjaHemat.
-        </p>
-      </main>
+      <ShuffleHero />
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -39,22 +39,22 @@ export function Navbar() {
         <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? '' : 'hidden'}`} id="navbar-sticky">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-normal border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="#home" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Beranda
               </Link>
             </li>
             <li>
-              <Link href="/profile" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="#profile" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Profil
               </Link>
             </li>
             <li>
-              <Link href="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="#contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Kontak
               </Link>
             </li>
             <li>
-              <Link href="/feedback" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="#feedback" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Kritik & Saran
               </Link>
             </li>
