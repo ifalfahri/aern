5d2cbd42d2069432950a15de44d3711206902e2e
IfalEXZ <ifaltampan69@gmail.com>
2025-01-09 12:33:36 +0800

    feat: add navigation link to home in FeedbackLayout component

--- app/feedback/layout.tsx
+++ app/feedback/layout.tsx
@@ -4,6 +4,7 @@ import { Footer } from "@/components/footer";
 import Aernime from "@/components/icons/aernime";
 import { LoadingScreen } from "@/components/loading-screen";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function FeedbackLayout({
   children,
@@ -24,8 +25,9 @@ export default function FeedbackLayout({
   return (
     <div className="min-h-screen bg-[url('https://res.cloudinary.com/dyjxcujz4/image/upload/v1717142252/layered-waves-haikei_7_olvnqc.svg')] bg-cover">
       <main className="container mx-auto flex flex-col items-center justify-center py-6">
+        <Link href="/">
         <Aernime className="h-24 mt-2 mb-8" />
-
+        </Link>
         <FeedbackTabs>{children}</FeedbackTabs>
       </main>
       <Footer />
