9421fbf059915fe7cb530e3e588cea4733620401
ifalfahri <ifaltampan69@gmail.com>
2025-04-22 08:08:49 +0800

    refactor: reorder imports and enhance metadata structure in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-import { Inter } from "next/font/google";
 import { Metadata } from "next";
+import { Inter } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
 
-// If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -11,7 +10,58 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Aern Store",
-  description: "Aern Store merupakan sebuah website dari toko online yang menjual berbagai macam barang dengan harga terjangkau dengan mottonya #SahabatBelanjaHemat. Berlokasi di Sulawesi Tengah, Indonesia",
+  description:
+    "Aern Store merupakan sebuah website dari toko online yang menjual berbagai macam barang dengan harga terjangkau dengan mottonya #SahabatBelanjaHemat. Berlokasi di Sulawesi Tengah, Indonesia",
+  keywords: [
+    "toko online",
+    "belanja hemat",
+    "Sulawesi Tengah",
+    "e-commerce",
+    "Aern Store",
+    "Aern",
+    "#SahabatBelanjaHemat",
+    "Toili",
+    "Toili Barat",
+    "Uwelolu",
+    "Unit 26",
+    "Desa Uwelolu",
+    "toko online toili",
+  ],
+  authors: [{ name: "Aldo", url: "https://www.aern.web.id" }],
+  creator: "Aldo",
+  publisher: "Aldo",
+  metadataBase: new URL("https://aern.web.id"),
+  alternates: {
+    canonical: "/",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: "Aern Store - #SahabatBelanjaHemat",
+    description:
+      "Aern Store merupakan sebuah website dari toko online yang menjual berbagai macam barang dengan harga terjangkau dengan mottonya #SahabatBelanjaHemat. Berlokasi di Sulawesi Tengah, Indonesia",
+    url: "https://aern.web.id",
+    siteName: "Aern Store",
+    type: "website",
+    locale: "id_ID",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Aern Store",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
 };
 
 export default function RootLayout({
@@ -28,9 +78,7 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
       </head>
-      <body>
-        {children}
-        </body>
+      <body>{children}</body>
     </html>
   );
 }
