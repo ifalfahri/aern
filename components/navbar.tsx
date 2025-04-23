3f3664c81b0bf6a5913cfda212b9184a4aa275cc
ifalfahri <ifaltampan69@gmail.com>
2025-04-23 12:31:47 +0800

    refactor: update navbar links for improved navigation clarity

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -116,18 +116,18 @@ export function Navbar() {
             </li>
             <li>
               <Link
-                href="#profile"
+                href="#features"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-colors"
               >
-                Profil
+                Fitur
               </Link>
             </li>
             <li>
               <Link
-                href="#contact"
+                href="#about"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-colors"
               >
-                Kontak
+                Tentang Kami
               </Link>
             </li>
             <li>
